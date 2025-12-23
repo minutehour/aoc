2c4eec0e929ba6533b7107b795067297a47db785
mhsn <mail@mhsn.net>
2025-12-23 19:15:05 +0000

    25-07 python p2

--- 2025/07/python.py
+++ 2025/07/python.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
 from fileinput import input
-from functools import partial, reduce
+from functools import cache
 
 lines = [line.strip() for line in input()]
+start = lines[0].index("S")
 
-bs = {lines[0].index("S")}
 silver = 0
+bs = {start}
+for line in lines:
+    for b in bs.copy():
+        if line[b] == "^":
+            silver += 1
+            bs -= {b}
+            bs |= {b - 1, b + 1}
 
 
-def split(t: int, c: int) -> set[int]:
-    global silver
-    if lines[t][c] == "^":
-        silver += 1
-        return {c - 1, c + 1}
-    return {c}
+@cache
+def tls(r: int, c: int) -> int:
+    if r == len(lines):
+        return 1
+    if lines[r][c] == "^":
+        return tls(r, c - 1) + tls(r, c + 1)
+    return tls(r + 1, c)
 
 
-for t, _ in enumerate(lines):
-    bs = reduce(set.union, map(partial(split, t), bs))
-
-
-gold = 0
+gold = tls(0, start)
 
 print("silver:", silver)
 print("gold:", gold)
